d9462c36e2ab6b3046c2dae5695cd66988fc9dcc
Ho Ngoc Giang <giaq1412@gmail.com>
2022-10-23 14:09:35 +0700

    Xu ly nut cong

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -30,9 +30,11 @@
         {
             this.lb_SoA = new System.Windows.Forms.Label();
             this.lb_SoB = new System.Windows.Forms.Label();
-            this.txt_SoA = new System.Windows.Forms.TextBox();
+            this.txtSoA = new System.Windows.Forms.TextBox();
             this.txtSoB = new System.Windows.Forms.TextBox();
             this.btnCong = new System.Windows.Forms.Button();
+            this.lbKetQua = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lb_SoA
@@ -48,19 +50,19 @@
             // lb_SoB
             // 
             this.lb_SoB.AutoSize = true;
-            this.lb_SoB.Location = new System.Drawing.Point(80, 113);
+            this.lb_SoB.Location = new System.Drawing.Point(80, 117);
             this.lb_SoB.Name = "lb_SoB";
             this.lb_SoB.Size = new System.Drawing.Size(74, 32);
             this.lb_SoB.TabIndex = 0;
             this.lb_SoB.Text = "Số b: ";
             this.lb_SoB.Click += new System.EventHandler(this.lb_SoB_Click);
             // 
-            // txt_SoA
+            // txtSoA
             // 
-            this.txt_SoA.Location = new System.Drawing.Point(191, 55);
-            this.txt_SoA.Name = "txt_SoA";
-            this.txt_SoA.Size = new System.Drawing.Size(444, 39);
-            this.txt_SoA.TabIndex = 1;
+            this.txtSoA.Location = new System.Drawing.Point(191, 55);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(444, 39);
+            this.txtSoA.TabIndex = 1;
             // 
             // txtSoB
             // 
@@ -77,6 +79,24 @@
             this.btnCong.TabIndex = 2;
             this.btnCong.Text = "Cộng ";
             this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // lbKetQua
+            // 
+            this.lbKetQua.AutoSize = true;
+            this.lbKetQua.Location = new System.Drawing.Point(80, 179);
+            this.lbKetQua.Name = "lbKetQua";
+            this.lbKetQua.Size = new System.Drawing.Size(112, 32);
+            this.lbKetQua.TabIndex = 0;
+            this.lbKetQua.Text = "Kết Quả: ";
+            this.lbKetQua.Click += new System.EventHandler(this.lb_SoB_Click);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(191, 172);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(444, 39);
+            this.txtKetQua.TabIndex = 1;
             // 
             // Form1
             // 
@@ -84,8 +104,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.txtKetQua);
             this.Controls.Add(this.txtSoB);
-            this.Controls.Add(this.txt_SoA);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.lbKetQua);
             this.Controls.Add(this.lb_SoB);
             this.Controls.Add(this.lb_SoA);
             this.Name = "Form1";
@@ -99,8 +121,10 @@
 
         private Label lb_SoA;
         private Label lb_SoB;
-        private TextBox txt_SoA;
+        private TextBox txtSoA;
         private TextBox txtSoB;
         private Button btnCong;
+        private Label lbKetQua;
+        private TextBox txtKetQua;
     }
 }
